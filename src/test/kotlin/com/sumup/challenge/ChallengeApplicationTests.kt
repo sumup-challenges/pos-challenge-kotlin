e2bbfe20dbb06169b156028f365f0f59f27af853
krzykrucz <krzysztof.kruczynski@sumup.com>
2023-11-13 20:40:10 +0100

    empty project

---
+++ src/test/kotlin/com/sumup/challenge/ChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sumup.challenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
